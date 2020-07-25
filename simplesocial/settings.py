c56868b16830b2e56ffbaed2c14d21d585a1b4a5
dhruvGupta12 <dhruv.gdps@gmail.com>
2020-07-25 19:07:22 +0530

    Added Heroku

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,4 +127,8 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 
 LOGIN_REDIRECT_URL='test'
 LOGOUT_REDIRECT_URL='thanks'
+
+
+# Activate Heroku
+django_heroku.settings(locals())
 
